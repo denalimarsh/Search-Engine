90faab6fe97cdf6582aa8ac5e5730286abb5d8dc
Denali Marsh <denalimarsh@gmail.com>
2016-10-26 17:36:28 -0700

    commit for code review

--- src/InvertedIndex.java
+++ src/InvertedIndex.java
@@ -1,32 +1,145 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.NavigableSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class InvertedIndex {
 
-	// TODO Move your nested treemap as a member (godzilla)
-	private TreeMap<String, TreeMap<String, TreeSet<Integer>>> godzilla;
-	
-	// TODO Move your add method
-	
+	private static TreeMap<String, TreeMap<String, TreeSet<Integer>>> invertedIndexy;
+
 	public InvertedIndex() {
-		godzilla = new TreeMap<>();
+		invertedIndexy = new TreeMap<String, TreeMap<String, TreeSet<Integer>>>();
 	}
 	
-	public void add(String word, String file, int position) {
+	/**
+	 * 
+	 * Adds a node containing the word, file the word is located in,
+	 * and the position of the word within the file to an inverted index
+	 * 
+	 * @param wordUpper - the cleaned word from the parseFile function
+	 * @param file - the file which the specified word is located within
+	 * @param position - the location of the word within the designated file
+	 */
+	public void add(String wordUpper, String file, int position) {
 		
+		String word = wordUpper.toLowerCase();
+		
+		if (invertedIndexy.get(word) == null) {
+			invertedIndexy.put(word, new TreeMap<String, TreeSet<Integer>>());
+			invertedIndexy.get(word).put(file, new TreeSet<Integer>());
+			invertedIndexy.get(word).get(file).add(position);
+		} else {
+			if (invertedIndexy.get(word).get(file) == null) {
+				invertedIndexy.get(word).put(file, new TreeSet<Integer>());
+				invertedIndexy.get(word).get(file).add(position);
+			} else {
+				invertedIndexy.get(word).get(file).add(position);
+			}
+		}
+	}
+
+	/**
+	 * Prints the inverted index by iterating through the TreeMap of words,
+	 * the TreeMap of files, and the TreeSet of integer positions, writing 
+	 * them to the designated output location in a pretty print JSON format
+	 * 
+	 * @param index - the complete inverted index, populated with data
+	 * @param path - the designated location which the function will print to
+	 * @throws IOException - the exception through if the bufferedReader is 
+	 * 						 unable to print to the path
+	 */
+	public static void print(InvertedIndex index, Path path) throws IOException {
+			
+		try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path, Charset.forName("UTF-8"));) {
+			bufferedWriter.write("{\n");
+			
+			int holderOne = 0;
+			for (String key : index.getKeySet()) {
+				if (key.compareTo("") != 0) {
+					bufferedWriter.write("\t" + quote(key) + ": {\n");
+					
+					TreeMap<String, TreeSet<Integer>> mappy = index.getKey(key);
+					int holderTwo = 0;
+					for (String fileName : mappy.navigableKeySet()) {
+						bufferedWriter.write("\t\t" + quote(fileName) + ": [\n");
+					
+						TreeSet<Integer> setty = mappy.get(fileName);
+						int holderThree = 0;
+						for (int a : setty) {
+							if (holderThree == setty.size() - 1) {
+								bufferedWriter.write("\t\t\t" + a + "\n");
+							} else {
+								bufferedWriter.write("\t\t\t" + a + ",\n");
+							}
+							holderThree++;
+						}
+						if (holderTwo == mappy.navigableKeySet().size() - 1) {
+							bufferedWriter.write("\t\t]\n");
+						} else {
+							bufferedWriter.write("\t\t],\n");
+						}
+						holderTwo++;
+					}
+					if (holderOne == index.getKeySet().size() - 1) {
+						bufferedWriter.write("\t}\n");
+					} else {
+						bufferedWriter.write("\t},\n");
+					}
+					holderOne++;
+				}
+			}
+			bufferedWriter.write("}");		
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	/**
+	 * Returns a navigable keySet from the inverted index
+	 * 
+	 * @return the navigable keySet
+	 */
+	public NavigableSet<String> getKeySet(){
+		return invertedIndexy.navigableKeySet();
+	}
+	
+	/**
+	 * 
+	 * @param key - the word which is mapped to the second treeMap
+	 * @return the second keyMap
+	 */
+	public TreeMap<String, TreeSet<Integer>> getKey(String key){
+		TreeMap<String, TreeSet<Integer>> mappy = invertedIndexy.get(key);
+		return mappy;
+	}
+	
+	/**
+	 * 
+	 * @param word - the word to be searched for in the inverted index
+	 * @return boolean true or false for contains word
+	 */
 	public boolean containsWord(String word) {
 		return false;
 	}
 	
-	public int size() {
-		return -1;
+	/**
+	 * Adds quotes to any string, useful in pretty printing JSON
+	 * 
+	 * @param text - the string to be quoted
+	 * @return the string with quotations on it
+	 */
+	public static String quote(String text) {
+		return String.format("\"%s\"", text);
 	}
 	
 	@Override
 	public String toString() {
-		return godzilla.toString();
+		return invertedIndexy.toString();
 	}
-	
 }
--- src/InvertedIndexBuilder.java
+++ src/InvertedIndexBuilder.java
@@ -1,18 +1,83 @@
-public class InvertedIndexBuilder {
-	/*
-	 * TODO
-	public static void traverse(Path directory, InvertedIndex index) {
-		for every file
-			if directory
-				recursive call
-			else if text file
-				parseFile(...)
-	}
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-	public static void parseFile(Path file, InvertedIndex index) {
-		loop through the file
-			parse the line
-				index.add(word, file, position);
+public class InvertedIndexBuilder {
+	
+	/**
+	 * Traverses the input directory recursively, locating all folders and files inside.
+	 * If the file is a text file, call the "parseFile" function
+	 * 
+	 * @param originalPath - the original input directory to be searched
+	 * @param index - the inverted index to be added to
+	 * @param outPath - the final destination for the inverted index to be written to
+	 */
+	public static void traverse(Path originalPath, InvertedIndex index, Path outPath) {
+
+		 try (DirectoryStream<Path> listing = Files.newDirectoryStream(originalPath)) {
+			 for (Path path : listing) {
+	                
+	                File fileName = path.toFile();
+                	String extension = "";
+                	int i =  (fileName.toString()).lastIndexOf('.');
+                	if (i > 0) {
+                	    extension = fileName.toString().substring(i+1);
+                	}
+	                if (Files.isDirectory(path)) {
+	                    traverse(path, index, outPath);
+	                }
+	                else if (extension.equals("txt")||extension.equals("TXT")){
+	                		parseFile(path, index);
+	                }
+	         }
+			 if(outPath != null){
+				 InvertedIndex.print(index, outPath);
+			 }
+		 } catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	*/
-}
+	
+	/**
+	 * 
+	 * Reads a text file in line by line, 'cleaning' the words as it goes by removing
+	 * all non-alphanumeric characters. For each cleaned word, add it to the inverted 
+	 * index.
+	 * 
+	 * @param path - the text file to be read in
+	 * @param index - the inverted index to be added to
+	 * @throws IOException - thrown if the bufferedReader is unable to write to the 
+	 * 						 designated path
+	 */
+	public static void parseFile(Path path, InvertedIndex index) throws IOException {
+
+		int positionHolder = 0;
+		Path path1 = path;
+		
+			try (BufferedReader br = Files.newBufferedReader(path, Charset.forName("UTF-8"))) {
+		        String line = br.readLine();
+				while ((line) != null) {
+					String[] words = line.split(" ");
+					String x = null;
+					for (int i = 0; i < words.length; i++) {
+						String holder = words[i];
+						x = holder.replaceAll("\\p{Punct}+", "");
+						String m = x.trim();
+						if (m.compareTo("") != 0) {
+							positionHolder++;
+							index.add(m, path1.normalize().toString(), positionHolder);
+						}
+					}
+					line = br.readLine();
+				}
+				br.close();			
+			} catch (IOException ex) {
+				System.out.println(ex.toString());
+				System.out.println("Could not find file " + path1.toString());
+			}
+	    }
+	}
